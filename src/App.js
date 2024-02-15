357142be655c5f60415018eab017ebe89d87c3f0
boysimon10 <simondiouf2710@gmail.com>
2024-02-15 14:56:18 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './pages/Home';
+import {Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+        <Route path="/" element={<Home />} />
+    </Routes>
+  )
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Page Home</h1>
+      <p className="text-lg">Route Test</p>
+
+    </div>
+  )
+}
+
+export default Home;
